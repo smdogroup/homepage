04b77f7ed9a658b1b09b597e4313bec76ec4e999
aaronyicongfu <fuyicong1996@gmail.com>
2021-10-29 15:15:50 -0400

    change copyright to SMDO group; add one example entry of previous students

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Structures and Multidisciplinary Design Optimization Research Group'
-copyright = '2021, Aaron Fu'
-author = 'Aaron Fu'
+copyright = '2021, SMDO Group'
+author = 'SMDO Group'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
